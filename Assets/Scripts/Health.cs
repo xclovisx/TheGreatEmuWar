ca065e4b8a17120fb3055ba8c1fa49db4a9d36f2
unknown <abdellbaz@gmail.com>
2018-05-17 12:03:00 +0200

    hpbar 0.1

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,47 +5,41 @@ using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    public float w = 1f;
+    public float y = 1f;
     public int t = 0;
     public Text countText;
     private int count;
 
     private void Start()
     {
-        transform.localScale = new Vector3(1, w, 1);
+        transform.localScale = new Vector3(1, y, 1);
         count = 100;
         countText.text = "Count:" + count.ToString();
     }
     void Update()
     {
-      
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Meteor")
+        if (y > 0 && t == 0)
         {
-            if (w > 0 && t == 0)
+
+            y -= 1f / 100;
+            count--;
+        }
+        else
+        {
+            t = 1;
+            y += 1f / 100;
+            count++;
+            if (y == 1f)
             {
-
-                w -= 1f / 100;
-                count--;
-            }
-            else
-            {
-                t = 1;
-                w += 1f / 100;
-                count++;
-                if (w == 1f)
-                {
-                    t = 0;
-                }
-
-
+                t = 0;
             }
 
-            countText.text = "Count:" + count.ToString();
-            transform.localScale = new Vector3(1, w, 1);
 
         }
+
+        countText.text = "Count:" + count.ToString();
+        transform.localScale = new Vector3(1, y, 1);
     }
-}
+   
+    }
+
