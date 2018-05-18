9f6cad233b85f3a3f36b75ebb4708e1faf9cb439
xclovisx <max.slotje@gmail.com>
2018-05-18 10:02:20 +0200

    Hey
    idk

---
+++ VR/Assets/Scripts/LifeCounter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LifeCounter : MonoBehaviour
+{
+
+    public Text countText;
+    public Text LoseText;
+
+    public int count;
+    public bool spawn;
+
+    void Start()
+    {
+        spawn = true;
+        count = 5;
+        SetCountText();
+        LoseText.text = "";
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Meteor" && count >= 1)
+        {
+            count = count - 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Lives: " + count.ToString();
+        if (count <= 0)
+        {
+            LoseText.text = "You Lose!";
+        }
+    }
+
+    private void Update()
+    {
+        if (count == 0)
+        {
+            spawn = false;
+        }
+    }
+}
