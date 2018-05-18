687f272994deba0a651fe15e9c8e3ff6957cec3d
Martin Keesom <25179@ma-web.nl>
2018-05-18 18:18:56 +0200

    Schieten werkend gekregen.
    Het schieten werkt moet alleen orientatie vragen aan leraar want dat wilt niet werken. Ver moet even al Max zijn spullen er in gezet worden dat ga ik dit weekend doen!

---
+++ Assets/Scripts/BulletRemover.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletRemover : MonoBehaviour {
+
+	void Update () {
+        Destroy(gameObject, 10);
+    }
+}
