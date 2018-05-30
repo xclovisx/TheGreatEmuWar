58338dd044abb4fbf66cda2f4a558f33c5ad56b1
Max Slot <max.slotje@gmail.com>
2018-05-30 09:48:36 +0200

    Mappie in mappie
    Want anders werkte het niet ofzo

---
+++ TheGreatEmuWar-1645129da52b08af4952462cce185327f8412a1d/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+
+    public Rigidbody m_Rigidbody;
+    public float m_Speed;
+
+    void Start () {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Speed = 10.0f;
+        m_Rigidbody.velocity = transform.up * m_Speed;
+    }
+	
+	void Update () {
+    }
+}
---
+++ TheGreatEmuWar-1645129da52b08af4952462cce185327f8412a1d/Assets/Scripts/meteorDeleter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteorDeleter : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        /*if (other.tag == "Player" || other.tag == "Bullet")
+        {*/
+            Destroy(gameObject);
+        //}
+    }
+}
