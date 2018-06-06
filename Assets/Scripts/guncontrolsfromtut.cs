c78920aea4e7072f522cb47b45ea7f8fcc9ffb2d
Martin Keesom <25179@ma-web.nl>
2018-06-06 09:32:22 +0200

    Nieuw gun implant
    Nieuwe gun staat er nettjes en fuctionerend er in

--- Assets/Scripts/guncontrolsfromtut.cs
+++ Assets/Scripts/guncontrolsfromtut.cs
@@ -21,7 +21,6 @@ public class guncontrolsfromtut : MonoBehaviour
     void Start()
     {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
-
     }
 
     void Update()
@@ -34,14 +33,15 @@ public class guncontrolsfromtut : MonoBehaviour
             GameObject Temporary_Bullet_Handler;
             Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position + new Vector3(-0.2f, 0.0f, 0.018f), Bullet_Emitter.transform.rotation) as GameObject;
 
-            Temporary_Bullet_Handler.transform.Rotate(Vector3.forward * 100);
+            Temporary_Bullet_Handler.transform.Rotate(Vector3.forward * 90);
+            Temporary_Bullet_Handler.transform.Rotate(Vector3.right * -90);
 
             Rigidbody Temporary_RigidBody;
             Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
 
             Temporary_RigidBody.AddForce(transform.up * Bullet_Up_Force);
 
-            Destroy(Temporary_Bullet_Handler, 2.0f);
+            Destroy(Temporary_Bullet_Handler, 5.0f);
 
         }
     }
