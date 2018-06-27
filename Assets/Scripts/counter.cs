acda03840929015e5f1db9186ac9bdc6e2a4d58d
unknown <abdellbaz@gmail.com>
2018-06-27 08:44:11 +0200

    hp ank kil 2x nu

--- Assets/Scripts/counter.cs
+++ Assets/Scripts/counter.cs
@@ -10,6 +10,8 @@ public class counter : MonoBehaviour {
     public static bool spawn;
     public Text kill;
     public Text hp;
+    public Text hp2;
+    public Text kill2;
 
     void Start () {
         spawn = true;
@@ -24,6 +26,8 @@ public class counter : MonoBehaviour {
         {
             kill.text = "P. " + killCounter.ToString();
             hp.text = "HP " + hpCounter.ToString();
+            hp2.text =  hpCounter.ToString();
+            kill2.text = killCounter.ToString();
             transform.localScale = new Vector3(1, hpCounter / 100, 1);
         }
         if (hpCounter <= 0) { spawn = false; }
