e9e6be56f6036391664c72031cd847d17feb1f57
Martin Keesom <25179@ma-web.nl>
2018-07-04 22:11:19 +0200

    Shield power up alleen nog ff artist die hun spullen er op moeten doen

---
+++ Assets/Scripts/PowerUpSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//  script gebruikt bij  {Spawners/EnemySpawner} script  bij EnemySpawner
+public class PowerUpSpawner : MonoBehaviour
+{
+    [Header("Prefab")]
+    public GameObject deleveryPrefab;
+
+    [Header("SpawnTime")]
+    public float spawnTime = 30f;
+
+    [Header("X Spawn Range")]
+    public float xMin;
+    public float xMax;
+
+    [Header("Y Spawn Range")]
+    public float yMin;
+    public float yMax;
+
+    [Header("Z Spawn Range")]
+    public float zMin;
+    public float zMax;
+
+    [Header("Speed")]
+    public int speed;
+
+    private bool stopSpawn;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        //print(counter.spawn);
+        if (counter.spawn) //will check if true
+        {
+            Vector3 SpawnPosition = new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), Random.Range(zMin, zMax));
+            GameObject Enemy = Instantiate(deleveryPrefab, SpawnPosition, Quaternion.identity);
+
+            EnemyMovement em = Enemy.GetComponent<EnemyMovement>();
+            em.target = GameObject.FindWithTag("AI Lock").transform;
+            em.speed = speed;
+
+            GoTo gt = Enemy.GetComponent<GoTo>();
+            gt.target = GameObject.FindWithTag("AI Lock").transform;
+        }
+    }
+
+
+    void Update()
+    { }
+
+}
---
+++ Assets/Scripts/ShieldSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+//  script gebruikt bij  {--} script bij Alien en Meteor
+
+
+public class ShieldSpawner : MonoBehaviour
+{
+
+    public GameObject Ship;
+
+    public GameObject Shield;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "AI Shield" || other.tag == "EBullet" || other.tag == "lasernt")
+        {
+            //Er gebeurt hier niks want Als hij deze raakt gebeurt er niks...
+        }
+
+        else
+        {
+            //Debug.Log("Pressed");
+            //The Explosion instantiation happens here.
+            GameObject Temporary_Explosion_Handler;
+            Temporary_Explosion_Handler = Instantiate(Shield, Shield.transform.position + new Vector3(0.0f, 0.0f, 0.0f), Shield.transform.rotation) as GameObject;
+
+            Temporary_Explosion_Handler.transform.Rotate(Vector3.forward * 90);
+            Temporary_Explosion_Handler.transform.Rotate(Vector3.right * -90);
+            Temporary_Explosion_Handler.transform.Rotate(Vector3.up * -90);
+
+            Rigidbody Temporary_RigidBody;
+            Temporary_RigidBody = Temporary_Explosion_Handler.GetComponent<Rigidbody>();
+
+            Destroy(Temporary_Explosion_Handler, 10.0f);
+
+
+        }
+    }
+}
