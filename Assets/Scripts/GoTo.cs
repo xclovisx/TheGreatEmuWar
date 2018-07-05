98df184e9a71376c4781aad56724e32126a3527c
Martin Keesom <25179@ma-web.nl>
2018-07-05 09:32:43 +0200

    AI Orbit fix!

--- Assets/Scripts/GoTo.cs
+++ Assets/Scripts/GoTo.cs
@@ -19,6 +19,16 @@ public class GoTo : MonoBehaviour
             transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
             transform.Translate(0, 0, 3 * Time.deltaTime);
         }
+        if (stop == 1)
+        {
+            Vector3 relativePos = (target.position + new Vector3(0, 0, 0)) - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+            Quaternion current = transform.localRotation;
+
+            transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
+            transform.Translate(3 * Time.deltaTime, 0, 0);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
