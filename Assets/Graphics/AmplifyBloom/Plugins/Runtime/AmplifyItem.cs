d72b8c5f91ceb4b2da54885ed7f093f07c385b50
Max <39294870+xclovisx@users.noreply.github.com>
2020-06-21 19:26:34 +0200

    Cleanup
    Yes yes, big clean-up
    
    *Deleted everything unneccesary
    *Also all the scripts because outdated :)
    
    Yes

--- Assets/Graphics/AmplifyBloom/Plugins/Runtime/AmplifyItem.cs
+++
@@ -1,9 +0,0 @@
-﻿// Amplify Bloom - Advanced Bloom Post-Effect for Unity
-// Copyright (c) Amplify Creations, Lda <info@amplify.pt>
-namespace AmplifyBloom
-{
-	interface IAmplifyItem
-	{
-		void Destroy();
-	}
-}
